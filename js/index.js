c02c557dd8682fe8750e601bb434a3ff9f71a2b0
litt-mei <1822131354@qq.com>
2020-11-23 22:18:16 +0800

    首页补充&login&goodsList页面

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
-document.querySelector('h1').onclick = function (){
-  alert(123);
-};
+var b_list = document.querySelector('.b_list');
+var l_list = document.querySelector('.l_list');
+
+
+b_list.onmouseenter = function() {
+    l_list.style.display = 'block'
+}
